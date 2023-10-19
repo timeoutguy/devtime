9ca65483630ec5a82760596e245642cd24154a2c
Marcus Vinícius <marcus.dev@protonmail.com>
2023-10-19 12:54:23 -0300

    feat(DT-0004): set colors variables

--- src/components/Mock.tsx
+++ src/components/Mock.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Home: React.FC = () => {
   return (
     <div>
-      <h1> Marcus </h1>
+      <h1 className="text-5xl"> Marcus </h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      "white": "#fffffe",
+      "purple": "#6246ea",
+      "charcoal": "#2b2c34",
+      "lavender-mist": "#d1d1e9"
+    },
     extend: {},
   },
   plugins: [],
