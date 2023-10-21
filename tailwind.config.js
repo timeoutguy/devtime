2ccedf0a48e160945c556dc6dab301dc20861325
Marcus Vinícius <marcus.dev@protonmail.com>
2023-10-20 23:50:37 -0300

    feat(DT-0005): Adds base navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
   theme: {
     colors: {
       "white": "#fffffe",
-      "purple": "#6246ea",
+      "purple": {
+        50: '#FAF5FF',
+        100: '#E9D8FD',
+        200: '#D6BCFA',
+        300: '#B794F4',
+        400: '#9F7AEA',
+        500: '#805AD5',
+        600: '#6B46C1',
+        700: '#553C9A',
+        800: '#44337A',
+        900: '#322659',
+      },
       "charcoal": "#2b2c34",
       "lavender-mist": "#d1d1e9"
     },
