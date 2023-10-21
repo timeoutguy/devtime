42ced6525fc135f8ace22623a672eb475dc564fa
Marcus Vinícius <marcus.dev@protonmail.com>
2023-10-21 14:30:19 -0300

    feat(DT-0008): Tooltip in search bar

---
+++ src/pages/home/components/Search/Search.tsx
@@ -0,0 +1,50 @@
+import { Box, Flex, Text, InputGroup, Input, InputRightElement, Tooltip } from '@chakra-ui/react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
+
+export const Search = () => {
+  const [selectedOption, setSelectedOption] = useState('top');
+
+  return (
+    <Box>
+      <Flex justifyContent="space-between" alignItems="center" mb={2}>
+        <Text className="font-bold"> Posts </Text>
+        <Flex>
+          <Text
+            onClick={() => setSelectedOption('top')}
+            color={selectedOption === 'top' ? 'white' : 'gray.500'}
+            bg={selectedOption === 'top' ? 'blackAlpha.800' : 'transparent'}
+            mr={1}
+            cursor="pointer"
+            px={4}
+            py={2}
+            borderRadius={50}
+            transition="ease-in 0.3s"
+          >
+            Top
+          </Text>
+          <Text
+            onClick={() => setSelectedOption('recent')}
+            color={selectedOption === 'recent' ? 'white' : 'gray.500'}
+            bg={selectedOption === 'recent' ? 'blackAlpha.800' : 'transparent'}
+            cursor="pointer"
+            px={4}
+            py={2}
+            borderRadius={50}
+            transition="ease-in 0.3s"
+          >
+            Recent
+          </Text>
+        </Flex>
+      </Flex>
+      <InputGroup>
+        <Input type="text" placeholder="What are you looking for..." />
+        <InputRightElement>
+          <Tooltip label="Search" aria-label="Search" hasArrow={true} placement="right">
+            <MagnifyingGlassIcon className="h-6 w-6 text-charcoal cursor-pointer" />
+          </Tooltip>
+        </InputRightElement>
+      </InputGroup>
+    </Box>
+  );
+};
