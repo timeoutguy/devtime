f8f7afe052d97cc6348f1546877d400bbe1af182
Marcus Vinícius <marcus.dev@protonmail.com>
2023-10-21 15:03:16 -0300

    feat(DT-0009): Adds post position

--- src/components/post-card/PostCard.tsx
+++ src/components/post-card/PostCard.tsx
@@ -1,20 +1,27 @@
-import { Box, Flex, Text, Link } from '@chakra-ui/react';
+import { Flex, Text, Link, Box, Divider } from '@chakra-ui/react';
 
 export const PostCard = () => {
   return (
-    <Box bgColor="gray.200" p={4}>
-      <Link href="/" fontWeight="bold" cursor="pointer">
-        Hi, Im looking for a COBOL developer
-      </Link>
-      <Flex alignItems="center" color="gray.500">
-        <Text textDecoration="underline" mr={2} cursor="pointer">
-          2 Comments
-        </Text>
-        <Text mr={2}> &middot; </Text>
-        <Text>
-          4 hrs ago by <Link textDecoration="underline"> Marcus Vinícius</Link>, Front end Developer
-        </Text>
-      </Flex>
-    </Box>
+    <Flex bgColor="gray.200" p={4} alignItems="center" mb={4}>
+      <Text color="gray.500" mr={2}>
+        1
+      </Text>
+      <Divider borderColor="gray.500" orientation="vertical" width="2" height="10"></Divider>
+      <Box>
+        <Link href="/" fontWeight="bold" cursor="pointer">
+          Hi, Im looking for a COBOL developer
+        </Link>
+        <Flex alignItems="center" color="gray.500">
+          <Text textDecoration="underline" mr={2} cursor="pointer">
+            2 Comments
+          </Text>
+          <Text mr={2}> &middot; </Text>
+          <Text>
+            4 hrs ago by <Link textDecoration="underline"> Marcus Vinícius</Link>, Front end
+            Developer
+          </Text>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
