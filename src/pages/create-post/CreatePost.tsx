9be86b1f069d88d73f86c4872d9ea9d24a3bfbf5
Marcus Vinícius <marcus.dev@protonmail.com>
2023-10-22 11:35:48 -0300

    feat(DT-0010): Creates 'create post' component page

---
+++ src/pages/create-post/CreatePost.tsx
@@ -0,0 +1,26 @@
+import { Box, Flex, Input, InputGroup, Link, Text } from '@chakra-ui/react';
+import React from 'react';
+
+import { Editor } from '../../components/editor';
+
+export const createPost: React.FC = () => {
+  return (
+    <Box>
+      <Text as="h1" fontSize="2xl" fontWeight="bold" mb={4}>
+        Create a new post
+      </Text>
+      <InputGroup mb={4}>
+        <Input placeholder="Title" />
+      </InputGroup>
+      <Box data-color-mode="light" mb={4}>
+        <Editor />
+      </Box>
+      <Flex justifyContent="flex-end" alignItems="center">
+        <Link href="/">
+          <button className="text-gray-600 px-4 py-1 mr-4 hover:bg-gray-200 rounded">Cancel</button>
+        </Link>
+        <button className="bg-charcoal text-white px-4 py-1 rounded font-bold"> Publish </button>
+      </Flex>
+    </Box>
+  );
+};
