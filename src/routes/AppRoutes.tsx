2cc66b5161e662dfe292ccc6c59bffe85ae7452d
Marcus Vinícius <marcus.dev@protonmail.com>
2023-10-22 21:10:04 -0300

    refactor(DT-0011) Capitalize create post page component

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { Home, createPost } from '../pages';
+import { Home, CreatePost } from '../pages';
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" Component={Home} />
-        <Route path="/create" Component={createPost} />
+        <Route path="/create" Component={CreatePost} />
       </Routes>
     </Router>
   );
