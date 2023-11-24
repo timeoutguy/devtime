823843c58312d72a96943d80da413dfe7b1a8da5
Marcus Vinícius <marcus.dev@protonmail.com>
2023-11-24 20:06:53 -0300

    feat: creating new user

--- src/pages/sign-up/Sign-up.tsx
+++ src/pages/sign-up/Sign-up.tsx
@@ -1,2 +1,115 @@
+import {
+  Box,
+  Text,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from '@chakra-ui/react';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
+import { User, createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
+import { FormEvent, useState } from 'react';
+
+import { ReactComponent as GithubLogo } from '../../assets/svg/github-logo.svg';
+import { ReactComponent as GoogleLogo } from '../../assets/svg/google-logo.svg';
+
 export const SignUp = () => {
+  const [displayName, setDisplayName] = useState<string | null>(null);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const auth = getAuth();
+
+  const handleSignUp = (event: FormEvent) => {
+    event.preventDefault();
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        changeDisplayNameOnFirebase(result.user);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const changeDisplayNameOnFirebase = (user: User) => {
+    updateProfile(user, {
+      displayName,
+    });
+  };
+
+  return (
+    <Box justifyContent="center" alignItems="center" display="flex" flexDirection="column" p={4}>
+      <Text as="h1" fontSize="2xl" fontWeight="bold" mb={4}>
+        Sign Up
+      </Text>
+      <form className="flex flex-col w-96" onSubmit={handleSignUp}>
+        <FormControl mb={4}>
+          <FormLabel> Display name </FormLabel>
+          <Input
+            type="text"
+            placeholder="John Doe"
+            size="lg"
+            onChange={(event) => setDisplayName(event.target.value)}
+          />
+        </FormControl>
+        <FormControl mb={4}>
+          <FormLabel> E-mail </FormLabel>
+          <Input
+            type="email"
+            placeholder="email@devtime.io"
+            size="lg"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </FormControl>
+        <FormControl mb={4}>
+          <FormLabel> Password </FormLabel>
+          <InputGroup>
+            <Input
+              type={showPassword ? 'text' : 'password'}
+              placeholder="**********"
+              size="lg"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <InputRightElement
+              cursor="pointer"
+              onClick={() => setShowPassword(!showPassword)}
+              h="full"
+            >
+              {showPassword ? (
+                <EyeSlashIcon width="20px" height="20px" />
+              ) : (
+                <EyeIcon width="20px" height="20px" />
+              )}
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+        <Button colorScheme="green" type="submit" mb={4}>
+          Sign up
+        </Button>
+        <h2 className="text-between-lines mb-4">
+          <span className="text-gray-500"> Or </span>
+        </h2>
+      </form>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDir="column"
+        className="w-96"
+      >
+        <Button width="100%" mb={4} leftIcon={<GoogleLogo width="24" height="24" />}>
+          Sign up with Google
+        </Button>
+        <Button
+          colorScheme="none"
+          className="bg-charcoal hover:bg-gray-900"
+          width="100%"
+          leftIcon={<GithubLogo width="24" height="24" />}
+        >
+          Sign up with Github
+        </Button>
+      </Box>
+    </Box>
+  );
 };
