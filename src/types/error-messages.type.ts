ad939fd62b0d5407efce3eb76873bd9abdfca73b
Marcus Vinícius <marcus.dev@protonmail.com>
2023-11-27 16:53:28 -0300

    feat: adds new error messages

--- src/types/error-messages.type.ts
+++ src/types/error-messages.type.ts
@@ -5,5 +5,12 @@ export interface ErrorMessageMap {
 export const loginErrorMessage: ErrorMessageMap = {
   'auth/internal-error': 'A internal error ocurred. Try again later',
   'auth/invalid-login-credentials': 'Wrong password',
-  'auth/invalid-email': 'Please input a valid email address',
+  'auth/invalid-email': 'A valid email address must be provided',
+};
+
+export const signUpErrorMessage: ErrorMessageMap = {
+  'auth/invalid-email': 'A valid email address must be provided',
+  'auth/missing-password': 'A password must be provided',
+  'auth/weak-password': 'Password should be at least 6 characters',
+  'auth/email-already-in-use': 'Email already in use',
 };
